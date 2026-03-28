0cfd1e9e9d183bf2e7923e0112dfe56119ed1721
Jasmine <jasmineoliver158@gmail.com>
2026-03-28 19:02:46 -0400

    createPost function

--- scripts.js
+++ scripts.js
@@ -6,8 +6,39 @@ let form = document.getElementById('post-form');
 
 
 
-function createPost() {
-    form.textContent = "";
+function createPost(e) {
+    e.preventDefault();
 
+    const blog = {
+        Id: crypto.randomUUID(),
+        Title: title.value,
+        Message: message.value,
+        TimeStamp: new Date().getTime()
+    }
+    
+    let isValid = true;
+
+    if (blog.Title === "") {
+        title.setCustomValidity("Please enter a title");
+        title.reportValidity();
+        isValid = false;
+    }else{
+        title.setCustomValidity = "";
+    }
+
+    if (blog.Message === "") {
+        message.setCustomValidity("Please enter a message");
+        message.reportValidity();
+        isValid = false;
+    }
+
+    if(isValid) {
+        posts.push(blog);
+
+        localStorage.setItem('blog', JSON.stringify(posts));
+
+        form.reset();
+        
+    }
     
 }
