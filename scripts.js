e508ff49cd8b1a1a0cbbec8df2c5896f22de3989
Jasmine <jasmineoliver158@gmail.com>
2026-03-28 18:33:30 -0400

    var declared

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let posts = [];
+let title = document.getElementById('title');
+let message = document.getElementById('message');
+let form = document.getElementById('post-form');
+
+
+
+
+function createPost() {
+    form.textContent = "";
+
+    
+}
